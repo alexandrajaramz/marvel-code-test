22d858ec0e953973531cc314ba2cb2a9b258b864
alexandrajaramz <alexandra.jara.mz@gmail.com>
2020-10-14 19:40:51 +0200

    Run build to publish

---
+++ docs/precache-manifest.650725e7b74795c5a87a5fbd54102984.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b775d96083256cc53af688b7706faa4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a62555170758d2e05dd",
+    "url": "./static/css/main.c96a08db.chunk.css"
+  },
+  {
+    "revision": "37381173d008c318fe35",
+    "url": "./static/js/2.618ff145.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.618ff145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a62555170758d2e05dd",
+    "url": "./static/js/main.b70d8198.chunk.js"
+  },
+  {
+    "revision": "96b136d1a62b9ee79cd2",
+    "url": "./static/js/runtime-main.7d514c39.js"
+  },
+  {
+    "revision": "78f339d10d0a8b7d84dc5d4e797e4971",
+    "url": "./static/media/mavel-logo.78f339d1.png"
+  }
+]);
